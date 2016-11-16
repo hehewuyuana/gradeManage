e9d34cf84e95bc1b5a54dc1c7b3e5f0a58526846
Xin Liao <xliao.abcft@niub.la>
2016-11-16 19:01:46 +0800

    1

--- app/js/grade.js
+++ app/js/grade.js
@@ -1,3 +1,21 @@
+//toastr插件配置
+toastr.options = {  
+    closeButton: false,  
+    debug: false,  
+    progressBar: false,  
+    positionClass: "toast-top-center",  
+    onclick: null,  
+    showDuration: "100",  
+    hideDuration: "1000",  
+    timeOut: "0",  
+    extendedTimeOut: "1000",  
+    showEasing: "swing",  
+    hideEasing: "linear",  
+    showMethod: "fadeIn",  
+    hideMethod: "fadeOut"  
+};  
+
+
 $('.list-group a').click(function(){
 	$('.list-group a').removeClass('active');
 	$(this).addClass('active');
@@ -10,6 +28,7 @@ app.config(function($routeProvider){
 	$routeProvider
 		.when('/personInfo',{
 			templateUrl:'pages/personInfo.html',
+			controller:'personInfoController'
 		})
 		.otherwise({
 			redirectTo:'/personInfo'
@@ -17,3 +36,31 @@ app.config(function($routeProvider){
 });
 
 angular.bootstrap(document,['myApp']);
+
+app.controller('personInfoController',function($http,$scope){
+	$http({
+		method:'POST',
+		url:'json/personInfo.json',
+		headers: {
+					'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+		},
+	}).success(function(data,status,headers,config,statusText){
+		//toastr({title:'Success',priority:'success',message:'good nice'});
+		$scope.$apply(function(){
+			var newData=JSON.parse(data);
+			$scope.num=newData.num;
+			$scope.name=newData.name;
+			$scope.sex=newData.sex;
+			$scope.age=newData.age;
+			$scope.class=newData.class;
+			$scope.major=newData.major;
+			$scope.institute=newData.institute;
+			$scope.grade=newData.grade;
+			toastr.success('good nice');
+		});
+	}).error(function(data,status,headers,config,statusText){
+		//toastr({title:'Error',priority:'error',message:'bad luck'});
+		toastr.error('bad luck');
+	});
+});
+
